7e6cdd80e755cc085c3013e3fd527f2e7b666dbc
reparaz01 <jreparazm@gmail.com>
2023-11-07 09:35:34 +0100

    t

---
+++ src/Router.js
@@ -0,0 +1,32 @@
+import {createRouter, createWebHistory} from 'vue-router';
+import DepartamentosComponent from './components/DepartamentosComponent.vue';
+import CreateDepartamento from './components/CreateDepartamento.vue';
+import DetallesDepartamento from './components/DetallesDepartamento.vue';
+import UpdateDepartamento from './components/UpdateDepartamento.vue';
+import DeleteDepartamento from './components/DeleteDepartamento.vue';
+
+
+const myRoutes = [
+    {
+        path: "/", component: DepartamentosComponent
+    }, 
+    {
+        path: "/create", component: CreateDepartamento
+    }, 
+    {
+        path: "/details/:id/:nombre/:localidad", component: DetallesDepartamento
+    },
+    {
+        path: "/update/:id", component: UpdateDepartamento
+    },
+    {
+        path: "/delete/:id", component: DeleteDepartamento
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: myRoutes
+})
+
+export default router;
---
+++ src/components/DeleteDepartamento.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1> Eliminar Departamento</h1>
+</template>
+
+<script>
+import ServiceDepartamentos from './../services/ServiceDepartamentos';
+const service = new ServiceDepartamentos();
+
+export default {
+ name :"DeleteDepartamento",
+ mounted() {
+    var id = this.$route.params.id;
+    service.deleteDepartamento(id).then(result => {
+        console.log(result);
+        this.$router.push("/");
+    })
+ }
+}
+</script>
+
+
+<style>
+
+</style>
